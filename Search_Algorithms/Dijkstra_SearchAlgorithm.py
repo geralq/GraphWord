5e4f92c4654a29c231f592bb125fc50a29a7327a
Gerardo <gerardoleoquin@gmail.com>
2024-11-26 18:58:11 +0000

    Adding Terraform and small changes.

--- Search_Algorithms/Dijkstra_SearchAlgorithm.py
+++ Search_Algorithms/Dijkstra_SearchAlgorithm.py
@@ -4,6 +4,13 @@ import networkx as nx
 
 class Dijkstra_SearchAlgorithm(SearchAlgorithm):
     def search(self, start, goal, graph):
+        if start not in graph.nodes:
+            print(f"Error: The start word '{start}' is not in the graph.")
+            return None
+        if goal not in graph.nodes:
+            print(f"Error: The goal word '{goal}' is not in the graph.")
+            return None
+
         try:
 
             path = nx.dijkstra_path(graph, origin, destination, weight='weight')
---
+++ Terraform/S2/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-west-1"
+}
+
+variable "ami" {
+  default = "ami-0c55b159cbfafe1f0"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
